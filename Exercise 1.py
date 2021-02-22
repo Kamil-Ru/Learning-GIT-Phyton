fc42c31da415f21c4858fc505d05ff206861d4a7
Hasysh <51496492+Hasysh@users.noreply.github.com>
2021-02-22 20:07:40 +0100

    Update Exercise 1.py

--- Exercise 1.py
+++ Exercise 1.py
@@ -3,6 +3,8 @@
 #Create a program that asks the user to enter their name and their age.
 #Print out a message addressed to them that tells them the year that they will turn 100 years old.
 
+#Importing the date
+import datetime
 
 #Asking fo name
 name = input("Hello, what is your name? ")
@@ -11,3 +13,12 @@ print ("Hello " + name)
 #Asking for a age
 age = input('How old you are ' + name + '? ')
 
+now = datetime.datetime.now()
+now_2 = now.year
+
+
+age_100 = str(now_2 + 100 - int(age))
+
+#print (age_100)
+
+print ('In ' + age_100 + ', you will be 100 years old.')
