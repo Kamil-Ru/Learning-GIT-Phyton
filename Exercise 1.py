20e2e66defd3b77ff856e2eaf86423b9b0615eba
Hasysh <51496492+Hasysh@users.noreply.github.com>
2021-02-22 19:38:48 +0100

    Create Exercise 1.py

---
+++ Exercise 1.py
@@ -0,0 +1,13 @@
+# Exercise 1
+
+#Create a program that asks the user to enter their name and their age.
+#Print out a message addressed to them that tells them the year that they will turn 100 years old.
+
+
+#Asking fo name
+name = input("Hello, what is your name? ")
+print ("Hello " + name)
+
+#Asking for a age
+age = input('How old you are ' + name + '? ')
+
