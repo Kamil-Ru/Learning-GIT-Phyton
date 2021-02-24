85198130c945873ac585a1e551e9d8471784bb19
Hasysh <51496492+Hasysh@users.noreply.github.com>
2021-02-24 21:11:38 +0100

    Add: extras in exercise

--- Exercise 1.py
+++ Exercise 1.py
@@ -1,7 +1,7 @@
 # Exercise 1
 
-#"Create a program that asks the user to enter their name and their age.
-#Print out a message addressed to them that tells them the year that they will turn 100 years old."
+#Create a program that asks the user to enter their name and their age.
+#Print out a message addressed to them that tells them the year that they will turn 100 years old.
 
 #Importing the date
 import datetime
@@ -16,8 +16,25 @@ age = input('How old you are ' + name + '? ')
 now = datetime.datetime.now()
 now_2 = now.year
 
-
 age_100 = str(now_2 + 100 - int(age))
 
+msg = 'In ' + age_100 + ', you will be 100 years old.'
+print (msg)
 
-print ('In ' + age_100 + ', you will be 100 years old.')
+
+# Extras 1 
+# Add on to the previous program by asking the user for another number and printing out that many copies of the previous message. (Hint: order of operations exists in Python)
+
+# Extras 2
+# Print out that many copies of the previous message on separate lines. (Hint: the string "\n is the same as pressing the ENTER button)
+
+number = int(input('Pleas ' + name + ', enter a number: '))
+
+print('Extras 1: ')
+
+# Alternative
+
+# for x in range(number):
+#  print ('In ' + age_100 + ', you will be 100 years old.')
+
+print ((msg + '\n' ) * number)
