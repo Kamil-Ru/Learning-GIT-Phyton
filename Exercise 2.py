581b332eb286a49311a7cb71f5dbfd46c2064aaa
Hasysh <51496492+Hasysh@users.noreply.github.com>
2021-02-24 21:11:59 +0100

    Add: extras in exercise

--- Exercise 2.py
+++ Exercise 2.py
@@ -1,20 +1,45 @@
-#Exercise 2
+# Exercise 2
 
-#"Ask the user for a number.
-#Depending on whether the number is even or odd,
-#print out an appropriate message to the user.
-#Hint: how does an even / odd number react differently when divided by 2?"
+# Ask the user for a number.
+# Depending on whether the number is even or odd,
+# print out an appropriate message to the user.
+# Hint: how does an even / odd number react differently when divided by 2?
 
-#Asking fo name
+# Asking fo name
 name = input("Hello, what is your name? ")
 
-#Asking for a number
+# Asking for a number
 number = int(input('Hello ' + name + '. Please enter a number: ' ))
 
-#Checking the number
+# Checking the number
 a = number % 2
 
 if a == 0:
- print (name + ' number ' + str(number) + ' is even.')
+    print (name + ' number ' + str(number) + ' is even.')
 else:
- print (name + ' number ' + str(number) + ' is odd.')
+    print (name + ' number ' + str(number) + ' is odd.')
+
+# If the number is a multiple of 4, print out a different message.
+
+b = number % 4
+
+if b == 0:
+    print (name + ' number ' + str(number) + ' is a multiple of 4.')
+else:
+    print (name + ' number ' + str(number) + ' is not a multiple of 4.')
+
+#  Ask the user for two numbers: 
+# - one number to check (call it num) 
+# - and one number to divide by (check). 
+#  If check divides evenly into num, tell that to the user. 
+#  If not, print a different appropriate message.
+
+number_1 = int(input(name + 'Enter a frist number: ' ))
+number_2 = int(input(name + 'Enter a secend number: ' ))
+
+c = number_1 % number_2
+
+if c == 0:
+    print (name + ' number ' + str(number_1) + ' is a multiple of ' + str(number_2) + '.')
+else:
+    print (name + ' number ' + str(number_1) + ' is not a multiple of ' + str(number_2) + '.')
