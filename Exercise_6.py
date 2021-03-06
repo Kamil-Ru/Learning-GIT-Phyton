50e3da4c5d74df6fa107143e74c44bca1de0fd2d
Hasysh <51496492+Hasysh@users.noreply.github.com>
2021-03-06 16:36:02 +0100

    code review :)

--- Exercise_6.py
+++ Exercise_6.py
@@ -4,7 +4,6 @@
 #   Ask the user for a string and print out whether this string is a palindrome or not. 
 #   (A palindrome is a string that reads the same forwards and backwards.)
 #   
-
 name = input('Hello, whats is your name?\n')
 string = input('Please ' + name + ' write any word: ')
 string_array = []
@@ -12,15 +11,11 @@ string_array = []
 string = string.lower()
 
 for x in string:
-    if x == ' ': continue
-    elif x == ',': continue
+    if x in [' ', ',', '.']: continue
     else:
         string_array.append(x)
-
-string_array_2 = list(string_array)
-string_array_2.reverse()
-
-if string_array == string_array_2:
+       
+if string_array == string_array[::-1]:
     print('The word is a palindrome !!!')
 else:
     print('The word is NOT a palindrome !!!')
